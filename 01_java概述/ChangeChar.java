9473dcdbb14817e516b0da80b23268b8802e2a1e
dengwenj <927561237@qq.com>
2022-06-02 17:22:00 +0800

    转义字符

---
+++ 01_java概述/ChangeChar.java
@@ -0,0 +1,10 @@
+public class ChangeChar {
+  public static void main(String[] args) {
+    System.out.println("上海\t北京\t重庆\t天津");
+    System.out.println("上海\n北京\n重庆\n天津");
+    System.out.println("上海\\北京\\重庆\\天津");
+    System.out.println("上海\"北京\"重庆\"天津");
+    System.out.println("你好啊\r上海"); // 上海啊
+    System.out.println("姓名\t年龄\t性别\n邓文杰\t22\t男");
+  }
+}
