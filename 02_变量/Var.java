b181df41d3a2cd50a5b5f92c0f4e2e75163c12ec
dengwenj <927561237@qq.com>
2022-06-03 16:36:59 +0800

    变量的基本使用

---
+++ 02_变量/Var.java
@@ -0,0 +1,16 @@
+public class Var {
+  public static void main(String[] args) {
+    // 变量相当于内存只中一个数据存储空间的表示
+    // 类型 + 名称 + 值
+    String name = "dengwenjie";
+    char gender = '男';
+    int age = 22;
+    double score = 99.5;
+
+    System.out.println("信息：");
+    System.out.println(name);
+    System.out.println(gender);
+    System.out.println(age);
+    System.out.println(score);
+  }
+}
