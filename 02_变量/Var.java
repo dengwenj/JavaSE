cbd5e6a38c83a004afb71d035ff27df69cb2a6c3
dengwenj <927561237@qq.com>
2022-07-18 22:29:52 +0800

    认识变量

--- 02_变量/Var.java
+++ 02_变量/Var.java
@@ -3,7 +3,7 @@ public class Var {
     // 变量相当于内存只中一个数据存储空间的表示
     // 类型 + 名称 + 值
     String name = "dengwenjie";
-    char gender = '男';
+    char gender = '男'; // 字符型
     int age = 22;
     double score = 99.5;
 
@@ -12,5 +12,10 @@ public class Var {
     System.out.println(gender);
     System.out.println(age);
     System.out.println(score);
+
+    // int num = 1l; 从long转换到int可能会有损失
+    long num1 = 1L;
+    System.out.println(num);
   }
 }
+ 
