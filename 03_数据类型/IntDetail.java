9c8b66be2728d599854799464bab752e4f80acbe
dengwenj <927561237@qq.com>
2022-08-24 22:45:55 +0800

    整型类型

---
+++ 03_数据类型/IntDetail.java
@@ -0,0 +1,9 @@
+public class IntDetail {
+  public static void main(String[] args) {
+    int n = 2; // 4 个字节
+    // int n1 = 20L; // 报错
+    long n2 = 20L;
+    System.out.println(n2);
+    System.out.println(n);
+  }
+}
