e80c5918dc7b008c083b5366165c4606fd2eed11
dengwenj <927561237@qq.com>
2022-11-20 16:07:59 +0800

    类与对象初体验

---
+++ 07_面向对象基础/Object01.java
@@ -0,0 +1,26 @@
+public class Object01 {
+  public static void main(String[] args) {
+    // oop 面向对象，核心就是类与对象
+    // 类是抽象的，概念的，代表一类事物，比如：人类，猫类
+    // 对象是具体的，实际的，代表一个具体事物，即实例
+    // 类是对象的模板，对象是类的一个个体，对应一个实例
+    Cat cat1 = new Cat();
+    cat1.name = "hh";
+    cat1.age = 10;
+    cat1.color = "白色";
+
+    Cat cat2 = new Cat();
+    cat2.name = "xx";
+    cat2.age = 20;
+    cat2.color = "黑色";
+
+    System.out.println("猫的信息: " + cat1.name + cat1.age + cat1.color);
+    System.out.println("猫的信息: " + cat2.name + cat2.age + cat2.color);
+  }
+}
+
+class Cat {
+  String name;
+  int age;
+  String color;
+}
