74b249c6991ed7bcdc6b7442fbea98a35c15b1b6
dengwenj <927561237@qq.com>
2022-11-21 21:02:08 +0800

    递归机制

---
+++ 07_面向对象基础/Recursion01.java
@@ -0,0 +1,21 @@
+public class Recursion01 {
+  public static void main(String[] args) {
+    T t = new T();
+    t.test(4);
+  }
+}
+
+class T {
+  public void test(int n) {
+    // if(n > 2) {
+    //   test(n - 1);
+    // }
+    // System.out.println(n); // 2 3 4
+
+    if(n > 2) {
+      test(n - 1);
+    } {
+      System.out.println(n); // 2
+    }
+  }
+}
