06d694e28b3757577266708a249a68068ddaa13c
dengwenj <927561237@qq.com>
2022-11-30 21:32:23 +0800

    封装和构造器使用

--- 08_面向对象中级/src/com/encap/Encapsulation01.java
+++ 08_面向对象中级/src/com/encap/Encapsulation01.java
@@ -2,20 +2,36 @@ package com.encap;
 
 public class Encapsulation01 {
   public static void main(String[] args) {
-    person person = new person();
+    Person person = new Person();
     person.setName("邓文杰1666");
     System.out.println(person.getName());
     person.setAge(120);
     System.out.println(person.getAge());
     person.setSalary(50000);
     System.out.println(person.getSalary());
+
+    Person person1 = new Person("邓文杰", 140, 50000);
+    person1.info();
   }
 }
 
-class person {
+class Person {
   public String name;
   private int age; // 私有
   private double salary; // 私有
+
+  public Person() {}
+
+
+  public Person(String name, int age, double salary) {
+//    this.name = name;
+//    this.age = age;
+//    this.salary = salary;
+//    我们可以将 set 方法写在构造器中，这样仍然可以验证
+    this.setName(name);
+    this.setAge(age);
+    this.setSalary(salary);
+  }
 
   public String getName() {
 //    判断一些权限，权限够就给你返回 name
@@ -55,5 +71,9 @@ class person {
     }
     System.out.println("薪水没有超过1w我给你1w");
     this.salary = 10000;
+  }
+
+  public void info() {
+    System.out.println(this.name + this.age + this.salary);
   }
 }
